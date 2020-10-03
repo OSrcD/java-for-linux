0b87bed3d4eae443b28c09f06d98f9b19912460c
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-10-03 20:43:15 +0800

    21-使用PostMan调试Restful接口

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
@@ -3,6 +3,7 @@ package com.imooc.controller;
 import com.imooc.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,24 @@ public class StuFooController {
     public Object getStu(int id){
         return stuService.getStuInfo(id);
     }
+
+    @PostMapping("/saveStu")
+    public Object saveStu(){
+        stuService.saveStu();
+        return "OK";
+    }
+
+    @PostMapping("/updateStu")
+    public Object updateStu(int id){
+        stuService.updateStu(id);
+        return "OK";
+    }
+
+    @PostMapping("/deleteStu")
+    public Object deleteStu(int id) {
+        stuService.deleteStu(id);
+        return "OK";
+    }
+
+
 }
--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImp.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImp.java
@@ -20,18 +20,27 @@ public class StuServiceImp implements StuService {
         return stuMapper.selectByPrimaryKey(id);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void saveStu() {
-
+        Stu stu = new Stu();
+        stu.setName("jack");
+        stu.setAge(19);
+        stuMapper.insert(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void updateStu(int id) {
-
+        Stu stu = getStuInfo(id);
+        stu.setName("lucy");
+        stu.setAge(20);
+        stuMapper.updateByPrimaryKey(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void deleteStu(int id) {
-
+        stuMapper.deleteByPrimaryKey(id);
     }
 }

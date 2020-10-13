1ec56331bdb7c8d6f5617ec9e3df441eee48ec02
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-10-13 21:50:02 +0800

    70-购物车 - 删除商品业务讲解

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/ShopcartController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/ShopcartController.java
@@ -34,4 +34,23 @@ public class ShopcartController {
         return IMOOCJSONResult.ok();
 
     }
+
+
+    @ApiOperation(value="从购物车中删除商品",notes="从购物车中删除商品",httpMethod="POST")
+    @PostMapping("/del")
+    public IMOOCJSONResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response){
+
+
+        if(StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)){
+           return IMOOCJSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+
+        return IMOOCJSONResult.ok();
+    }
 }

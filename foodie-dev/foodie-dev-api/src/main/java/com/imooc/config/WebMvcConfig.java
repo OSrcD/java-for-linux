03a61a2aff63d9003654231d30d78b2b61980390
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-10-20 01:15:01 +0800

    109-上传头像 - 为静态资源提供网络映射服务

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
@@ -4,9 +4,19 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 实现静态资源的映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)  {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/") // 映射swagger2
+                .addResourceLocations("file:/Users/virtualman/Downloads/temp/workspace/images/"); // 映射本地静态资源
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {

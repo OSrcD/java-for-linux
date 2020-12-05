ad8a53491ff00ecf578364e07bd8d0bda7bcf57f
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-12-05 18:21:45 +0800

    222-Redis 操作工具类讲解

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/RedisController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller;
 
+import com.imooc.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,25 +14,30 @@ import springfox.documentation.annotations.ApiIgnore;
 public class RedisController {
 
     @Autowired
-
-
     private RedisTemplate redisTemplate;
+
+
+    @Autowired
+    private RedisOperator redisOperator;
+
 
     @GetMapping("/set")
     public Object set(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+//        redisTemplate.opsForValue().set(key, value);
+        redisOperator.set(key, value);
         return "OK";
     }
 
     @GetMapping("/get")
     public String get(String key) {
-        String value = (String) redisTemplate.opsForValue().get(key);
-        return value;
+//        String value = (String) redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
 
     @GetMapping("/delete")
     public Object delete(String key) {
-        redisTemplate.delete(key);
+//        redisTemplate.delete(key);
+        redisOperator.del(key);
         return "OK";
     }
 }

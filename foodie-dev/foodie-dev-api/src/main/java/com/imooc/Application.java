6f79855513cef8332ba83a586f052485268efc0b
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-12-25 16:17:52 +0800

    254-SpringSession实现用户会话

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,16 +2,19 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages="com.imooc.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages={"com.imooc","org.n3r.idworker"})
 @EnableScheduling       // 开启定时任务
+@EnableRedisHttpSession // 开启使用redis作为spring session
 public class Application {
 
     public static void main(String[] args) {
---
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        return "ok";
+    }
+}

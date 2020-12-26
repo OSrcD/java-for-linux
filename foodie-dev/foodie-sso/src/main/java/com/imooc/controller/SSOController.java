b35ab89c205fe2c3e46cd146cc29d76a41260ef6
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-12-26 18:54:12 +0800

    260-CAS单点登录 - 跳转至登录

--- foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
+++ foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -1,22 +1,27 @@
 package com.imooc.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SSOController {
 
+    @GetMapping("/login")
+    public String login(String returnUrl ,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public Object hello() {
-        return "Hello World~";
+        model.addAttribute("returnUrl", returnUrl);
+
+        // TODO 后续完善校验是否登录
+
+        // 用户从未登录过，第一次进入侧跳转到CAS的统一登录页面
+
+        return "login";
     }
 }

e4d5bd02404117eb227efa86d9ca8401e13cf660
OSrcD <OpenDeveloper@mywiz.cn>
2020-12-28 20:09:29 +0800

    261-CAS单点登录 - 用户登录与校验

--- foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
+++ foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -1,14 +1,34 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.vo.UsersVO;
+import com.imooc.service.UserService;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.MD5Utils;
+import com.imooc.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+import java.util.UUID;
 
 @Controller
 public class SSOController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+    private String REDIS_USER_TOKEN = "redis_user_token";
 
     @GetMapping("/login")
     public String login(String returnUrl ,
@@ -21,6 +41,42 @@ public class SSOController {
         // TODO 后续完善校验是否登录
 
         // 用户从未登录过，第一次进入侧跳转到CAS的统一登录页面
+
+        return "login";
+    }
+
+    @PostMapping("/doLogin")
+    public String doLogin(String username,
+                        String password,
+                        String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) throws Exception {
+
+        model.addAttribute("returnUrl", returnUrl);
+
+        // 0. 判断用户名和密码必须不为空
+        if(StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password)){
+            model.addAttribute("errmsg","用户名或密码不能为空");
+            return "login";
+        }
+
+        // 1. 实现登录
+        Users userResult = userService.queryUserForLogin(username,
+                MD5Utils.getMD5Str(password));
+        if (userResult == null) {
+            model.addAttribute("errmsg", "用户名或密码不正确");
+            return "login";
+        }
+
+        // 实现用户的redis会话
+        String uniqueToken = UUID.randomUUID().toString().trim();
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(userResult, usersVO);
+        usersVO.setUserUniqueToken(uniqueToken);
+        redisOperator.set(REDIS_USER_TOKEN + ":" + userResult.getId(),
+                JsonUtils.objectToJson(usersVO));
 
         return "login";
     }

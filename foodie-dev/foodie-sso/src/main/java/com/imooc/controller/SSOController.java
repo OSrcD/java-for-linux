0052b8a0d3b37b6f8029a09e0689372fc8659e0b
OSrcD <OpenDeveloper@mywiz.cn>
2020-12-29 22:47:49 +0800

    262-CAS单点登录 - 创建用户会话、全局门票、临时票据

--- foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
+++ foodie-dev/foodie-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
@@ -28,7 +29,11 @@ public class SSOController {
     @Autowired
     private RedisOperator redisOperator;
 
-    private String REDIS_USER_TOKEN = "redis_user_token";
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+    public static final String REDIS_USER_TICKET = "redis_user_ticket";
+    public static final String REDIS_TMP_TICKET = "redis_tmp_ticket";
+
+    public static final String COOKIE_USER_TICKET = "cookie_user_ticket";
 
     @GetMapping("/login")
     public String login(String returnUrl ,
@@ -45,6 +50,13 @@ public class SSOController {
         return "login";
     }
 
+    /**
+     * CAS的统一登录接口
+     *      目的：
+     *          1. 登录后创建用户的全局会话                    -> uniqueToken
+     *          2. 创建用户全局门票，用以表示在CAS端是否登录     -> userTicket
+     *          3. 创建用户的临时票据，用于回跳回传             -> tmpTicket
+     */
     @PostMapping("/doLogin")
     public String doLogin(String username,
                         String password,
@@ -70,7 +82,7 @@ public class SSOController {
             return "login";
         }
 
-        // 实现用户的redis会话
+        // 2. 实现用户的redis会话
         String uniqueToken = UUID.randomUUID().toString().trim();
         UsersVO usersVO = new UsersVO();
         BeanUtils.copyProperties(userResult, usersVO);
@@ -78,6 +90,46 @@ public class SSOController {
         redisOperator.set(REDIS_USER_TOKEN + ":" + userResult.getId(),
                 JsonUtils.objectToJson(usersVO));
 
+        // 3. 生成ticket门票，全局门票，代表用户在CAS端登录过
+        String userTicket = UUID.randomUUID().toString().trim();
+
+        // 3.1 用户全局门票需要放入CAS端的cookie中
+        setCookie(COOKIE_USER_TICKET,userTicket,response);
+
+        // 4. userTicket关联用户id，并且放入redis中，代表这个用户有门票了，可以在各个景区游玩
+        redisOperator.set(REDIS_USER_TICKET + ":" + userTicket,userResult.getId());
+
+        // 5. 生成临时票据，回跳到调用端网站，是由CAS端所签发的一个一次性的临时ticket
+        String tmpTicket = createTicket();
+
         return "login";
+//        return "redirect:" + returnUrl + "?tmpTicket=" + tmpTicket;
     }
+
+    /**
+     * 创建临时票据
+     * @return
+     */
+    private String createTicket() {
+        String tmpTicket = UUID.randomUUID().toString().trim();
+        try {
+            redisOperator.set(REDIS_TMP_TICKET + ":" + tmpTicket,
+                    MD5Utils.getMD5Str(tmpTicket),600);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tmpTicket;
+    }
+
+    private void setCookie(String key,
+                           String val,
+                           HttpServletResponse response) {
+
+        Cookie cookie = new Cookie(key,val);
+        cookie.setDomain("sso.com");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+    }
+
 }

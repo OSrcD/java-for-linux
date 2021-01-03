df4bc0cfa7b98d2e1404eddcacb39755e18aad38
OSrcD <OpenDevel@126.com>
2021-01-04 00:20:03 +0800

    修改md文件

--- markdown-toc/src/main/java/com/github/houbb/markdown/toc/MardownTocMain.java
+++ markdown-toc/src/main/java/com/github/houbb/markdown/toc/MardownTocMain.java
@@ -6,11 +6,11 @@ import com.github.houbb.markdown.toc.core.impl.AtxMarkdownToc;
 public class MardownTocMain {
 
     public static void main(String[] args) {
-        TocConstant.DEFAULT_TOC_HEAD = "";
+        TocConstant.DEFAULT_TOC_HEAD = "# 目录";
         AtxMarkdownToc.newInstance()
                 .charset("UTF-8")
                 .write(true)
                 .subTree(true).
-                genTocFile("java-alibaba-p7/阶段三：分布式架构-逐个击破分布式核心问题（9-17周）/第10周 分布式搜索引擎-Elasticsearch/276-mappings自定义创建映射/276-mappings自定义创建映射.md");
+                genTocFile("/Users/virtualman/IdeaProjects/java-for-linux/java-alibaba-p7/阶段三：分布式架构-逐个击破分布式核心问题（9-17周）/第10周 分布式搜索引擎-Elasticsearch/275-head与postman基于索引的基本操作/275-head与postman基于索引的基本操作.md");
     }
 }

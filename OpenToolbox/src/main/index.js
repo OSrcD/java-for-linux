d12498f6ba4585372fdf1f57261acd23013eeed5
OSrcD <OpenDevel@126.com>
2021-01-06 02:30:23 +0800

    优化OpenToolbox

--- OpenToolbox/src/main/index.js
+++ OpenToolbox/src/main/index.js
@@ -10,6 +10,8 @@ const winURL = process.env.NODE_ENV === 'development'
     : `file://${__dirname}/index.html`
 
 
+
+
 /**
  * 开启一个子窗口
  */
@@ -37,118 +39,108 @@ function subWindow() {
     subWindow.webContents.closeDevTools()
 }
 
-const Menus = [{
-    label: 'OpenToolbox',
-    submenu: [{
-        label: 'About OpenToolbox'
-    }]
-},
+
+const template = [
     {
-        label: 'Window',
-        submenu: [{
+        label: 'Edit',
+        submenu: [
+            {role: 'undo'},
+            {role: 'redo'},
+            {type: 'separator'},
+            {role: 'cut'},
+            {role: 'copy'},
+            {role: 'paste'},
+            {role: 'pasteandmatchstyle'},
+            {role: 'delete'},
+            {role: 'selectall'}
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {role: 'reload'},
+            {role: 'forcereload'},
+            {role: 'toggledevtools'},
+            {type: 'separator'},
+            {role: 'resetzoom'},
+            {role: 'zoomin'},
+            {role: 'zoomout'},
+            {type: 'separator'},
+            {role: 'togglefullscreen'}
+        ]
+    },
+    {
+        role: 'window',
+        submenu: [
+            {role: 'minimize'},
+            {role: 'close'}
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternal('https://electron.atom.io'); }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            {role: 'about'},
+            {type: 'separator'},
+            {role: 'services', submenu: []},
+            {type: 'separator'},
+            {role: 'hide'},
+            {role: 'hideothers'},
+            {role: 'unhide'},
+            {type: 'separator'},
+            {role: 'quit'}
+        ]
+    });
+
+    // Edit menu
+    template[1].submenu.push(
+        {type: 'separator'},
+        {
+            label: 'Speech',
+            submenu: [
+                {role: 'startspeaking'},
+                {role: 'stopspeaking'}
+            ]
+        }
+    );
+
+    // Window menu
+    template[3].submenu = [
+        {role: 'close'},
+        {role: 'minimize'},
+        {role: 'zoom'},
+        {type: 'separator'},
+        {role: 'front'},
+        {
             label: 'Open Sub Window',
             click: function () {
                 subWindow();
             }
-        }]
-    }
-];
-
-app.on('ready', function () {
-    createWindow ();
-});
-
-
-    const template = [
-        {
-            label: 'Edit',
-            submenu: [
-                {role: 'undo'},
-                {role: 'redo'},
-                {type: 'separator'},
-                {role: 'cut'},
-                {role: 'copy'},
-                {role: 'paste'},
-                {role: 'pasteandmatchstyle'},
-                {role: 'delete'},
-                {role: 'selectall'}
-            ]
-        },
-        {
-            label: 'View',
-            submenu: [
-                {role: 'reload'},
-                {role: 'forcereload'},
-                {role: 'toggledevtools'},
-                {type: 'separator'},
-                {role: 'resetzoom'},
-                {role: 'zoomin'},
-                {role: 'zoomout'},
-                {type: 'separator'},
-                {role: 'togglefullscreen'}
-            ]
-        },
-        {
-            role: 'window',
-            submenu: [
-                {role: 'minimize'},
-                {role: 'close'}
-            ]
-        },
-        {
-            role: 'help',
-            submenu: [
-                {
-                    label: 'Learn More',
-                    click () { require('electron').shell.openExternal('https://electron.atom.io'); }
-                }
-            ]
         }
     ];
 
-    if (process.platform === 'darwin') {
-        template.unshift({
-            label: app.getName(),
-            submenu: [
-                {role: 'about'},
-                {type: 'separator'},
-                {role: 'services', submenu: []},
-                {type: 'separator'},
-                {role: 'hide'},
-                {role: 'hideothers'},
-                {role: 'unhide'},
-                {type: 'separator'},
-                {role: 'quit'}
-            ]
-        });
 
-        // Edit menu
-        template[1].submenu.push(
-            {type: 'separator'},
-            {
-                label: 'Speech',
-                submenu: [
-                    {role: 'startspeaking'},
-                    {role: 'stopspeaking'}
-                ]
-            }
-        );
+}
 
-        // Window menu
-        template[3].submenu = [
-            {role: 'close'},
-            {role: 'minimize'},
-            {role: 'zoom'},
-            {type: 'separator'},
-            {role: 'front'},
-            {
-                label: 'Open Sub Window',
-                click: function () {
-                    subWindow();
-                }
-            }
-        ];
-    }
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+
+app.on('ready', function () {
+});
+
+
 
 
 
@@ -178,8 +170,7 @@ function createWindow() {
      */
     // const mainMenu = Menu.buildFromTemplate(Menus);
     // Menu.setApplicationMenu(mainMenu);
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+
 
 
 

444f613df622dff8feabc597c6534acb9f9d95a7
OSrcD <OpenDevel@126.com>
2021-01-05 18:50:01 +0800

    修改开源笔记文件名

--- markdown-toc/src/main/java/com/github/houbb/markdown/toc/MardownTocApplication.java
+++ markdown-toc/src/main/java/com/github/houbb/markdown/toc/MardownTocApplication.java
@@ -4,13 +4,11 @@ import com.github.houbb.markdown.toc.constant.TocConstant;
 import com.github.houbb.markdown.toc.core.impl.AtxMarkdownToc;
 
 public class MardownTocApplication {
+    public MardownTocApplication() {
+    }
 
     public static void main(String[] args) {
         TocConstant.DEFAULT_TOC_HEAD = "# 目录";
-        AtxMarkdownToc.newInstance()
-                .charset("UTF-8")
-                .write(true)
-                .subTree(true).
-                genTocFile("开源笔记/java-alibaba-p7/阶段三：分布式架构-逐个击破分布式核心问题（9-17周）/第10周 分布式搜索引擎-Elasticsearch/283-建立ik中文分词器.md");
+        AtxMarkdownToc.newInstance().charset("UTF-8").write(true).subTree(true).genTocFile("开源笔记/java-alibaba-p7/阶段三：分布式架构-逐个击破分布式核心问题（9-17周）/第10周 分布式搜索引擎-Elasticsearch/282-分词与内置分词器.md");
     }
 }

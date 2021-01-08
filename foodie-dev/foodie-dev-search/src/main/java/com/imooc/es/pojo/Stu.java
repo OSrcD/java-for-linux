99f15fe0f5525f87d9d3d86acd0d1fd818596dd7
OSrcD <OpenDevel@126.com>
2021-01-08 13:01:37 +0800

    44 - Elasticsearch 整合 springboot - 索引的删除与 mappings 更新

--- foodie-dev/foodie-dev-search/src/main/java/com/imooc/es/pojo/Stu.java
+++ foodie-dev/foodie-dev-search/src/main/java/com/imooc/es/pojo/Stu.java
@@ -3,9 +3,10 @@ package com.imooc.es.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 // 文档就是一个对象
-@Document(indexName = "stu", type = "_doc")
+@Document(indexName = "stu", type = "_doc", shards = 3, replicas = 0)
 public class Stu {
 
     // spring boot 默认会生成与对象一样的值 _id 与 stuId 不写也可以 会自动随机生成
@@ -18,6 +19,15 @@ public class Stu {
 
     @Field(store = true)
     private Integer age;
+
+    @Field(store = true, type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private String description;
+
+    @Field(store = true)
+    private Float money;
 
     public Long getStuId() {
         return stuId;
@@ -35,11 +45,35 @@ public class Stu {
         this.name = name;
     }
 
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
     public Integer getAge() {
         return age;
     }
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
+++ foodie-dev/foodie-dev-search/src/test/java/com/test/ESTest.java
@@ -18,19 +18,35 @@ public class ESTest {
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
 
+    /**
+     * 不建议使用 ElasticsearchTemplate 对索引进行管理（创建索引，更新索引，删除索引）
+     * 索引就像是数据库或者数据库中的表，我们平时是不会通过Java代码频繁的去创建修改删除数据库或者表的
+     * 我们只会针对数据做CRUD的操作
+     * 在es中也是同理，我们尽量使用 ElasticsearchTemplate 对文档数据做CRUD的操作
+     * 1. 属性（FieldType）类型不灵活
+     * 2. 主分片与副本分片数无法设置
+     */
     @Test
     public void createIndexStu() {
 
         // 第一次会先创建索引 第二次以后只会新增数据
         Stu stu = new Stu();
-        stu.setStuId(1002L);
+        stu.setStuId(1003L);
         stu.setName("spider man");
         stu.setAge(22);
-
+        stu.setMoney(15.5f);
+        stu.setSign("i am spider man");
+        stu.setDescription("I wish i am spider man");
         // spring boot 会自动帮我们创建IndexQuery 只要把对象传入进行就行了
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(stu).build();
         // 创建索引
         elasticsearchTemplate.index(indexQuery);
 
     }
+
+    @Test
+    public void deleteIndexStu() {
+        elasticsearchTemplate.deleteIndex(Stu.class);
+    }
+
 }

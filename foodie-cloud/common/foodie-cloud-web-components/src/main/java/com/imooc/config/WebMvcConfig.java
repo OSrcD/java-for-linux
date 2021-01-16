b79cfa1f414c7374c38efb1a280021a01ded8660
OSrcD <OpenDevel@126.com>
2021-01-16 19:45:40 +0800

    22 - Eureka - 电商系统集成 Eureka 商品中心 - 3

--- foodie-cloud/common/foodie-cloud-web-components/src/main/java/com/imooc/config/WebMvcConfig.java
+++ foodie-cloud/common/foodie-cloud-web-components/src/main/java/com/imooc/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.imooc.config;
 
-import com.imooc.controller.interceptor.UserTokenInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,36 +27,36 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
-    public UserTokenInterceptor userTokenInterceptor() {
-        return new UserTokenInterceptor();
-    }
+//    @Bean
+//    public UserTokenInterceptor userTokenInterceptor() {
+//        return new UserTokenInterceptor();
+//    }
 
-    /**
-     * 注册拦截器
-     * @param registry
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry){
-
-        registry.addInterceptor(userTokenInterceptor())
-                .addPathPatterns("/hello")
-                .addPathPatterns("/shopcart/add")
-                .addPathPatterns("/shopcart/del")
-                .addPathPatterns("/address/list")
-                .addPathPatterns("/address/add")
-                .addPathPatterns("/address/update")
-                .addPathPatterns("/address/setDefault")
-                .addPathPatterns("/address/delete")
-                .addPathPatterns("/orders/*")
-                .addPathPatterns("/center/*")
-                .addPathPatterns("/userInfo/*")
-                .addPathPatterns("/myorders/*")
-                .addPathPatterns("/mycomments/*")
-                .excludePathPatterns("/myorders/deliver")
-                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
-
-        WebMvcConfigurer.super.addInterceptors(registry);
-    }
+//    /**
+//     * 注册拦截器
+//     * @param registry
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry){
+//
+//        registry.addInterceptor(userTokenInterceptor())
+//                .addPathPatterns("/hello")
+//                .addPathPatterns("/shopcart/add")
+//                .addPathPatterns("/shopcart/del")
+//                .addPathPatterns("/address/list")
+//                .addPathPatterns("/address/add")
+//                .addPathPatterns("/address/update")
+//                .addPathPatterns("/address/setDefault")
+//                .addPathPatterns("/address/delete")
+//                .addPathPatterns("/orders/*")
+//                .addPathPatterns("/center/*")
+//                .addPathPatterns("/userInfo/*")
+//                .addPathPatterns("/myorders/*")
+//                .addPathPatterns("/mycomments/*")
+//                .excludePathPatterns("/myorders/deliver")
+//                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
+//
+//        WebMvcConfigurer.super.addInterceptors(registry);
+//    }
 
 }
---
+++ foodie-cloud/domain/item/foodie-item-web/src/main/java/com/imooc/item/controller/ItemsController.java
@@ -0,0 +1,182 @@
+package com.imooc.item.controller;
+
+import com.imooc.controller.BaseController;
+import com.imooc.item.pojo.Items;
+import com.imooc.item.pojo.ItemsImg;
+import com.imooc.item.pojo.ItemsParam;
+import com.imooc.item.pojo.ItemsSpec;
+import com.imooc.item.pojo.vo.CommentsLevelCountsVO;
+import com.imooc.item.pojo.vo.ItemInfoVO;
+import com.imooc.item.pojo.vo.ShopcartVO;
+import com.imooc.item.service.ItemService;
+import com.imooc.pojo.IMOOCJSONResult;
+import com.imooc.pojo.PagedGridResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value="商品接口",tags={"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController extends BaseController {
+
+
+    @Autowired
+    private ItemService itemService;
+
+
+
+    @ApiOperation(value="查询商品详情",notes="查询商品详情",httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult info(
+            @ApiParam(name="itemId",value="商品id",required=true)
+            @PathVariable String itemId){
+
+        if(StringUtils.isEmpty(itemId))
+            return IMOOCJSONResult.ok(null);
+
+        Items items  = itemService.queryItemsById(itemId);
+        List<ItemsImg> itemsImgList = itemService.queryItemsImgList(itemId);
+        List<ItemsSpec> itemsSpecList = itemService.queryItemsSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemsParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(items);
+        itemInfoVO.setItemImgList(itemsImgList);
+        itemInfoVO.setItemSpecList(itemsSpecList);
+        itemInfoVO.setItemParams(itemsParam);
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+
+    @ApiOperation(value="查询商品评价等级",notes="查询商品评价等级",httpMethod="GET")
+    @GetMapping("/commentLevel")
+    public IMOOCJSONResult commentLevel(
+            @ApiParam(name="itemId",value="商品id",required = true)
+            @RequestParam String itemId){
+
+        if(StringUtils.isBlank(itemId)){
+            return IMOOCJSONResult.errorMsg(null);
+        }
+
+        CommentsLevelCountsVO countsVO = itemService.queryCommentsCounts(itemId);
+
+        return IMOOCJSONResult.ok(countsVO);
+    }
+
+    @ApiOperation(value="查询商品评论",notes="查询商品评论",httpMethod="GET")
+    @GetMapping("/comments")
+    public IMOOCJSONResult comments(
+            @ApiParam(name="itemId",value="商品id",required=true)
+            @RequestParam String itemId,
+            @ApiParam(name="level",value="评价等级",required=false)
+            @RequestParam Integer level,
+            @ApiParam(name="page",value="查询下一页的第几页",required=false)
+            @RequestParam Integer page,
+            @ApiParam(name="pageSize",value="分页的每一页显示的条数",required=false)
+            @RequestParam Integer pageSize){
+
+        if(StringUtils.isBlank(itemId)){
+            return IMOOCJSONResult.errorMsg(null);
+        }
+
+        if(page == null){
+            page = 1;
+        }
+
+        if(pageSize == null){
+            pageSize = COMMON_PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.queryPagedComments(itemId,
+                                                                level,
+                                                                page,
+                                                                pageSize);
+        return IMOOCJSONResult.ok(grid);
+    }
+
+//    @ApiOperation(value="搜索商品列表",notes="搜索商品列表",httpMethod="GET")
+//    @GetMapping("/search")
+//    public IMOOCJSONResult search(
+//            @ApiParam(name="keywords",value="关键字",required=true)
+//            @RequestParam String keywords,
+//            @ApiParam(name="sort",value="排序",required=false)
+//            @RequestParam String sort,
+//            @ApiParam(name="page",value="查询下一页的第几页",required=false)
+//            @RequestParam Integer page,
+//            @ApiParam(name="pageSize",value="分页的每一页显示的条数",required=false)
+//            @RequestParam Integer pageSize){
+//
+//        if(StringUtils.isBlank(keywords)){
+//            return IMOOCJSONResult.errorMsg(null);
+//        }
+//
+//        if(page == null){
+//            page = 1;
+//        }
+//
+//        if(pageSize == null){
+//            pageSize = PAGE_SIZE;
+//        }
+//
+//        PagedGridResult grid = itemService.searchItems(keywords,sort,page,pageSize);
+//
+//        return IMOOCJSONResult.ok(grid);
+//
+//    }
+//
+//    @ApiOperation(value="通过分类id搜索商品列表",notes="通过分类id搜索商品列表",httpMethod="GET")
+//    @GetMapping("/catItems")
+//    public IMOOCJSONResult catItems(
+//            @ApiParam(name="catId",value="三级分类id",required=true)
+//            @RequestParam Integer catId,
+//            @ApiParam(name="sort",value="排序",required=false)
+//            @RequestParam String sort,
+//            @ApiParam(name="page",value="查询下一页的第几页",required=false)
+//            @RequestParam Integer page,
+//            @ApiParam(name="pageSize",value="分页的每一页显示的条数",required=false)
+//            @RequestParam Integer pageSize){
+//
+//        if(catId == null){
+//            return IMOOCJSONResult.errorMsg(null);
+//        }
+//
+//        if(page == null){
+//            page = 1;
+//        }
+//
+//        if(pageSize == null){
+//            pageSize = PAGE_SIZE;
+//        }
+//
+//        PagedGridResult grid = itemService.searchItems(catId,
+//                sort,
+//                page,
+//                pageSize);
+//
+//        return IMOOCJSONResult.ok(grid);
+//
+//    }
+
+    // 用于用户长时间未登陆网站，刷新购物车中的数据（主要是商品价格），类似京东淘宝
+    @ApiOperation(value="根据商品规格ids查找最新的商品数据",notes="根据商品规格ids查找最新的商品数据",httpMethod="GET")
+    @GetMapping("/refresh")
+    public IMOOCJSONResult refresh(
+            @ApiParam(name="itemSpecIds",value="拼接的规格ids",required=true,example="1001,1003,1005")
+            @RequestParam String itemSpecIds){
+
+        if(StringUtils.isBlank(itemSpecIds)){
+            return IMOOCJSONResult.ok();
+        }
+
+        List<ShopcartVO> list = itemService.queryItemsBySpecIds(itemSpecIds);
+
+        return IMOOCJSONResult.ok(list);
+    }
+
+}

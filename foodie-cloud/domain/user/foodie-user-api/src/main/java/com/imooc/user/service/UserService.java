a5a27df12426946405b00e83022f807300a64f5e
OSrcD <OpenDevel@126.com>
2021-01-16 23:51:55 +0800

    23 - Eureka - 电商系统集成 Eureka 用户中心

---
+++ foodie-cloud/domain/user/foodie-user-api/src/main/java/com/imooc/user/service/UserService.java
@@ -0,0 +1,32 @@
+package com.imooc.user.service;
+
+import com.imooc.user.pojo.Users;
+import com.imooc.user.pojo.bo.UserBO;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("user-api")
+public interface UserService {
+
+    /**
+     * 判断用户名是否存在
+     * @param username
+     * @return
+     */
+    @GetMapping("user/exists")
+    public boolean queryUsernameIsExist(@RequestParam("username") String username);
+
+    /**
+     * 创建用户
+     * @param userBo
+     * @return
+     */
+    @PostMapping("user")
+    public Users createUser(@RequestBody UserBO userBo);
+
+    /**
+     * 检索用户名和密码是否匹配，用于登录
+     */
+    @GetMapping("verify")
+    public Users queryUserForLogin(@RequestParam("username") String username,
+                                   @RequestParam("password") String password);
+}

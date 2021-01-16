95a5680570eb34d414b9b123f92eb23d2552d2f2
OSrcD <OpenDevel@126.com>
2021-01-16 14:55:14 +0800

    20 - Eureka - 电商系统集成 Eureka 商品中心 - 1

---
+++ foodie-cloud/domain/item/foodie-item-mapper/src/main/java/com/imooc/item/mapper/ItemsCommentsMapper.java
@@ -0,0 +1,8 @@
+package com.imooc.item.mapper;
+
+
+import com.imooc.item.pojo.ItemsComments;
+import com.imooc.my.mapper.MyMapper;
+
+public interface ItemsCommentsMapper extends MyMapper<ItemsComments> {
+}

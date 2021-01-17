159caf590ee063c6c0870c0e28ab9d4b552ab675
OSrcD <OpenDevel@126.com>
2021-01-17 17:04:06 +0800

    03 - Ribbon - 给消费者添加负载均衡功能

--- java-for-alibaba/spring-cloud-demo/eureka/eureka-client/src/main/java/com/imooc/springcloud/Controller.java
+++ java-for-alibaba/spring-cloud-demo/eureka/eureka-client/src/main/java/com/imooc/springcloud/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 // 可以方便生成 Log 属性 不用声明logFactory
 @Slf4j
---
+++ java-for-alibaba/spring-cloud-demo/ribbon/ribbon-consumer/src/main/java/com/imooc/springcloud/Controller.java
@@ -0,0 +1,20 @@
+package com.imooc.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/sayHi")
+    public String sayHi() {
+        return restTemplate.getForObject(
+                "http://eureka-client/sayHi",
+                String.class);
+
+    }
+}

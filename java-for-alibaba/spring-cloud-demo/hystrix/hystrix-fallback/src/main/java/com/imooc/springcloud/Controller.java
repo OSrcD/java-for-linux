dd1b67e27eb6be8dde9244d64b123eccb51a972e
OSrcD <OpenDevel@126.com>
2021-01-22 17:13:53 +0800

    04 - Hystrix - 实现 Request Cache 减压

--- java-for-alibaba/spring-cloud-demo/hystrix/hystrix-fallback/src/main/java/com/imooc/springcloud/Controller.java
+++ java-for-alibaba/spring-cloud-demo/hystrix/hystrix-fallback/src/main/java/com/imooc/springcloud/Controller.java
@@ -1,5 +1,8 @@
 package com.imooc.springcloud;
 
+import com.imooc.springcloud.hystrix.RequestCacheService;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import lombok.Cleanup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,8 @@ public class Controller {
     @Autowired
     private MyService myService;
 
+    @Autowired
+    private RequestCacheService requestCacheService;
 
     @GetMapping("/fallback")
     public String fallback() {
@@ -20,6 +25,21 @@ public class Controller {
     @GetMapping("/timeout")
     public String timeout(Integer timeout) {
         return myService.retry(timeout);
+    }
+
+    @GetMapping("/cache")
+    public Friend cache(String name) {
+
+        @Cleanup HystrixRequestContext context =
+                HystrixRequestContext.initializeContext();
+
+        Friend friend = requestCacheService.requestCache(name);
+        name += "!";
+        friend = requestCacheService.requestCache(name);
+        return friend;
+
+
+
     }
 
 

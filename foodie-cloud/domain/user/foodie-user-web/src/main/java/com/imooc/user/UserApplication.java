49251c7be2cffa92bb1cf43e253d977b8bfa8146
OSrcD <OpenDevel@126.com>
2021-01-24 22:39:23 +0800

    12 - Hystrix - 用户模块 基于HystrixCommand 注解配置降级和线程池

--- foodie-cloud/domain/user/foodie-user-web/src/main/java/com/imooc/user/UserApplication.java
+++ foodie-cloud/domain/user/foodie-user-web/src/main/java/com/imooc/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.imooc.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -13,6 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 // TODO feign注解
 public class UserApplication {
 

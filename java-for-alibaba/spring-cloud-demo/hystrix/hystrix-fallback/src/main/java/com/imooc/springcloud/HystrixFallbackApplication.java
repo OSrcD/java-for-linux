4bf113dc53dd44f0d7b9c17df2bf736905d612d9
OSrcD <OpenDevel@126.com>
2021-01-24 16:53:15 +0800

    09 - Hystrix - Turbine 集成大盘监控 - 2

--- java-for-alibaba/spring-cloud-demo/hystrix/hystrix-fallback/src/main/java/com/imooc/springcloud/HystrixFallbackApplication.java
+++ java-for-alibaba/spring-cloud-demo/hystrix/hystrix-fallback/src/main/java/com/imooc/springcloud/HystrixFallbackApplication.java
@@ -3,6 +3,7 @@ package com.imooc.springcloud;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;

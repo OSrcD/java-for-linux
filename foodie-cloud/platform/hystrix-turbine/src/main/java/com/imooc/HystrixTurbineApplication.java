89cc77dc0466cbd29a393d49fbf84c5c84b1f1ac
OSrcD <OpenDevel@126.com>
2021-01-24 19:03:50 +0800

    10 - Hystrix - 电商系统集成 Hystrix 基础组件 Turbine

---
+++ foodie-cloud/platform/hystrix-turbine/src/main/java/com/imooc/HystrixTurbineApplication.java
@@ -0,0 +1,21 @@
+package com.imooc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableAutoConfiguration
+public class HystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixTurbineApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+
+}

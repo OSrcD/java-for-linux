1d3dab70eff2739b8c456fbd0ca396571b20e3df
OSrcD <OpenDevel@126.com>
2021-01-24 16:53:15 +0800

    09 - Hystrix - Turbine 集成大盘监控 - 2

---
+++ java-for-alibaba/spring-cloud-demo/hystrix/hystrix-dashboard/src/main/java/com/imooc/springcloud/HystrixDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.springcloud;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@EnableHystrixDashboard
+@SpringCloudApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+
+    }
+}

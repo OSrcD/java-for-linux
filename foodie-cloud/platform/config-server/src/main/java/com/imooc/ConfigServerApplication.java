e4188374237bc56e660853b18e37ad6aad6f0a64
OSrcD <OpenDevel@126.com>
2021-01-26 19:52:05 +0800

    09 - config - 电商系统集成 Config 注册中心搭建和配置

---
+++ foodie-cloud/platform/config-server/src/main/java/com/imooc/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run();
+    }
+}

780220909ea2afb9030f5671fb3791ce6a2ab9db
OSrcD <OpenDevel@126.com>
2021-01-26 17:13:12 +0800

    06 - Config - 借助 Eureka 实现高可用性配置中心

--- java-for-alibaba/spring-cloud-demo/config/config-server-eureka/src/main/java/com/imooc/springcloud/ConfigServerApplication.java
+++ java-for-alibaba/spring-cloud-demo/config/config-server-eureka/src/main/java/com/imooc/springcloud/ConfigServerApplication.java
@@ -1,16 +1,18 @@
 package com.imooc.springcloud;
 
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigClientApplication {
+public class ConfigServerApplication {
+
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigClientApplication.class)
+        new SpringApplicationBuilder(ConfigServerApplication.class)
                 .web(WebApplicationType.SERVLET)
                 .run();
     }

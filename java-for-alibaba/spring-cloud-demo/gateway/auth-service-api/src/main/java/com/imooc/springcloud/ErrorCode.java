7eeb1ce65a9e88aee82f6a7a6981e3fb060d2d53
OSrcD <OpenDevel@126.com>
2021-01-28 17:23:16 +0800

    05 - Gateway - 基于 JWT 实现用户鉴权 - 1

---
+++ java-for-alibaba/spring-cloud-demo/gateway/auth-service-api/src/main/java/com/imooc/springcloud/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.imooc.springcloud;
+
+public class ErrorCode {
+    
+    public static final Long SUCCESS = 1L;
+
+    // 密码不正确
+    public static final Long INCORRECT_PWD = 1000L;
+
+    public static final Long USER_NOT_FOUND = 1001L;
+}

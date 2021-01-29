710c80c64d4e4e087f8b69470fbf809e9c615de7
OSrcD <OpenDevel@126.com>
2021-01-29 13:08:49 +0800

    08 - Gateway - 电商系统集成 Gateway 配置网关层 Redis 限流

--- foodie-cloud/platform/gateway/src/main/java/com/imooc/springcloud/RoutesConfiguration.java
+++ foodie-cloud/platform/gateway/src/main/java/com/imooc/springcloud/RoutesConfiguration.java
@@ -1,12 +1,24 @@
 package com.imooc.springcloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 public class RoutesConfiguration {
+
+    @Autowired
+    private KeyResolver hostNameResolver;
+
+    @Autowired
+    @Qualifier("redisLimiterUser") // 指定加载哪个bean
+    private RateLimiter rateLimiterUser;
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
@@ -18,6 +30,14 @@ public class RoutesConfiguration {
                 )
 
                 .route(r -> r.path("/address/**", "/passport/**", "/userInfo/**", "/center/**")
+                        // 指定 requestRateLimiter 过滤器
+                        .filters(f -> f.requestRateLimiter(c -> {
+                            // 指定限流Key
+                            c.setKeyResolver(hostNameResolver);
+                            // 指定限流规则
+                            c.setRateLimiter(rateLimiterUser);
+                            c.setStatusCode(HttpStatus.BAD_GATEWAY);
+                        }))
                         .uri("lb://FOODIE-USER-SERVICE")
                 )
 

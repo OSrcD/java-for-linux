da351e2e311ec022223d81f72325ce4cb36fd52d
OSrcD <OpenDevel@126.com>
2021-01-29 16:42:56 +0800

    11 - Gateway - 电商系统集成 Gateway 网关层登录校验 - 1

--- foodie-cloud/domain/user/foodie-user-web/src/main/java/com/imooc/user/UserApplication.java
+++ foodie-cloud/domain/user/foodie-user-web/src/main/java/com/imooc/user/UserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -15,6 +16,10 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+// 可以以 basePackageClasses 类的形式加载 也可以以 basePackages
+@EnableFeignClients(basePackages = {
+        "com.imooc.auth" //调用微服务auth 生成token
+})
 // TODO feign注解
 public class UserApplication {
 
--- foodie-cloud/platform/gateway/src/main/java/com/imooc/springcloud/GatewayApplication.java
+++ foodie-cloud/platform/gateway/src/main/java/com/imooc/springcloud/GatewayApplication.java
@@ -1,13 +1,18 @@
 package com.imooc.springcloud;
 
 
+import com.imooc.auth.service.AuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(basePackageClasses = {
+        AuthService.class
+})
 public class GatewayApplication {
 
     public static void main(String[] args) {
